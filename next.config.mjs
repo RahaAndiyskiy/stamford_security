e5f40b51f2abb3e95c0a2859adfd31cf0395e583
RahaAndiyskiy <rahaandiyskiy8974@gmail.com>
2026-05-20 20:36:19 +0300

    Update caching headers for Next image and static assets

--- next.config.mjs
+++ next.config.mjs
@@ -30,6 +30,24 @@ const nextConfig = {
         ],
       },
       {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/_next/image',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
         source: '/:path*',
         headers: [
           {
